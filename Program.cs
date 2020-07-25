5fe2565975d8c51f4a226896aa431d119d812f43
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-07-25 15:37:43 +0530

    Linked List

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Code_Challenge_Day_15_Linked_List
+{
+    class Program
+    {
+        public static Node insert(Node head, int data)
+        {
+           
+            if(head == null)
+            {
+                return new Node(data);
+            }
+
+            else if(head.next == null)
+            {
+                head.next = new Node(data);
+
+            }
+
+            else
+            {
+                insert(head.next, data);
+            }
+
+            return head;
+        }
+
+        public static void display(Node head)
+        {
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Node head = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                head = insert(head, data);
+            }
+            display(head);
+        }
+    }
+}
